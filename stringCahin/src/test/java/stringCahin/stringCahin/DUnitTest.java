1d29c55b097a2e3dadda9a1f7b080f9daefc3f0c
yanilood-ctrl <yanilood@gmail.com>
2025-11-17 18:45:29 +0200

    lastupdeat

--- stringCahin/src/test/java/stringCahin/stringCahin/DUnitTest.java
+++ stringCahin/src/test/java/stringCahin/stringCahin/DUnitTest.java
@@ -7,14 +7,10 @@ public class DUnitTest {
 
     @Test
     public void testDWithStub() {
-        // סטאב עבור E
         EStub stub = new EStub();
 
-        // יוצרים את D עם הסטאב
         D d = new D(stub);
 
-        // D מוסיפה "L" ל־"HEL" → "HELL"
-        // והסטאב מוסיף "X" → "HELLX"
         String result = d.process("HEL");
 
         assertEquals("HELLX", result);
